b2ccc39af84624637d39abd25950c00951b33f98
Prakash Rai <prakash.bahing21@gmail.com>
2023-04-20 17:41:48 +0545

    Specifying the text overheading for proper test and getByTitle query

--- src/components/Header/_test_/Header.test.js
+++ src/components/Header/_test_/Header.test.js
@@ -11,10 +11,35 @@ it("renders learn react header", () => {
   expect(headerElement).toBeInTheDocument();
 });
 
+/**
+ * Incase of having multiple headers this test will fail.
+ * 
+ *  <>
+      <h1 className="header">{title}</h1>
+      <h1 className="header">Cats</h1>
+    </>
+ */
+
+// it("should render same text passed into title prop", () => {
+//   render(<Header title={"My todo header"} />);
+//   const headerElement = screen.getByRole("heading");
+//   expect(headerElement).toBeInTheDocument();
+// });
+
+/**
+ *So for more specific test you can specify what text you want to get over heading role.
+ *You can do that by specifying an object and giving the name.
+ */
 it("should render same text passed into title prop", () => {
   render(<Header title={"My todo header"} />);
-  const headerElement = screen.getByRole("heading");
+  const headerElement = screen.getByRole("heading", {
+    name: "My todo header",
+  });
   expect(headerElement).toBeInTheDocument();
 });
 
-
+it("get by title", () => {
+  render(<Header title={"My todo header"} />);
+  const headerElement = screen.getByTitle("header");
+  expect(headerElement).toBeInTheDocument();
+});
