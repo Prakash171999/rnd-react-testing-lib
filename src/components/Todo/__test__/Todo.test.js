bf865e145e43de5072423c0442a5f366395da8e6
Prakash Rai <prakash.bahing21@gmail.com>
2023-05-19 16:57:17 +0545

    Integra-test: Add todo feature test for multiple input handle

--- src/components/Todo/__test__/Todo.test.js
+++ src/components/Todo/__test__/Todo.test.js
@@ -10,8 +10,19 @@ const MockTodo = () => {
   );
 };
 
+const addTask = (tasks) => {
+  const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+  const buttonElement = screen.getByRole("button", { name: /Add/i });
+  tasks.forEach((task) => {
+    fireEvent.change(inputElement, {
+      target: { value: task },
+    });
+    fireEvent.click(buttonElement);
+  });
+};
+
 describe("todo", () => {
-  it("Todo", () => {
+  it("Add single Todo test", () => {
     render(<MockTodo />);
     const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
     const buttonElement = screen.getByRole("button", { name: /Add/i });
@@ -21,5 +32,12 @@ describe("todo", () => {
     fireEvent.click(buttonElement);
     const divElement = screen.getByText(/Go on Long Vacation/i);
     expect(divElement).toBeInTheDocument();
+  });
+
+  it("Todo loopable integration test", () => {
+    render(<MockTodo />);
+    addTask(["Go Grocery Shopping", "Go on vacation", "Complete your task"]);
+    const divElement = screen.getAllByTestId("task-container");
+    expect(divElement.length).toBe(3);
   });
 });
